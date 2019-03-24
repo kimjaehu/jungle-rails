ee6024fb19ec64406f79897be374a35a1fff1c31
kimjaehu <kimjaehu@gmail.com>
2019-03-24 03:40:17 +0000

    ADD feature product user rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,15 +2,20 @@ class ReviewsController < ApplicationController
 
   before_filter :authorize
   def create
-    @product = Product.find(params[:product_id])
-    @review = @product.reviews.new(review_params)
-    @review.user = current_user
     
+    # byebug
+    @product = Product.find(params[:product_id])
+    @review = Review.new(review_params)
+    @review.product = @product
+    @review.user = current_user   
+
     if @review.save
       redirect_to product_path(@product)
     else
+      # byebug
       render :new
     end
+
   end
 
   def destroy
@@ -18,10 +23,24 @@ class ReviewsController < ApplicationController
     @review.destroy
 
     redirect_to :back
+
   end
 
   private
     def review_params
-      params.require(:review).permit(:description, :rating)
+      params.require(:review).permit(
+        :description,
+        :rating,
+        :user_id,
+        :product_id
+      )
     end
+
+
+  # Add blank stars after rating however errors 'undefined method'
+  def blank_stars
+    5 - @reviews.rating.to_i
+  end
+  helper_method :blank_stars
+
 end
